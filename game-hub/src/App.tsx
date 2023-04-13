9e8212f3f9bd0e8b2f83ac2405b9f2608a529447
aleena-gigi <aleenagigi16@gmail.com>
2023-04-13 18:53:20 -0400

    Build a responsive layout

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 const App = () => {
   return (
-    <div>
-      <Button colorScheme="blue">Button</Button>
-    </div>
+    <Grid templateAreas={{ base: `"nav""main"`, lg: `"nav nav""aside main"` }}>
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      {/* to make sure aside is only shown on large devices we must wrap it inside show */}
+      {/* it has prop above which means the content inside it will only be shown on the screen size above the mentioned  */}
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 };
 
