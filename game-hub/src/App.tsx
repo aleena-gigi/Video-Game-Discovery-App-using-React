52a3e8a4e8713c15d29af6dbd868749b7a90b55a
aleena-gigi <aleenagigi16@gmail.com>
2023-04-13 18:17:16 -0400

    installed chakra

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import { Button, ButtonGroup } from "@chakra-ui/react";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
 };
 
 export default App;
